e11cce3f068abc686e0461febe35bbef147dc447
Anandb <141913589+Anandb2003@users.noreply.github.com>
2026-03-26 13:14:34 +0530

    Delete Advance-Quiz-Application-main directory

--- Advance-Quiz-Application-main/quiz-app/provided.js
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-
-// import Images and Icons for the projects
-
--- Advance-Quiz-Application-main/quiz-app/src/config_url.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL="http://localhost:8080";
-
-const api_calls={
-    LOGIN:"${BASE_URL}/",
-}
